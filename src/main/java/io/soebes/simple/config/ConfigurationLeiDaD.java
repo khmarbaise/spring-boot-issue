112a62d05890ba241cb2cb4b49ae6d23e3fd7dcb
Karl Heinz Marbaise <kama@soebes.de>
2021-03-24 16:05:03 +0100

    Cleaned up example.

--- src/main/java/io/soebes/simple/config/ConfigurationLeiDaD.java
+++ src/main/java/io/soebes/simple/config/ConfigurationLeiDaD.java
@@ -16,38 +16,12 @@ public class ConfigurationLeiDaD {
 
   private static final String DEFAULT_INBOUND = "inbound";
 
-  private static final String DEFAULT_IMPORTER = "importer";
-
-  private static final String DEFAULT_ARCHIVE = "archive";
-
-  private static final String DEFAULT_EXPORT = "export";
-
-  /**
-   * Defines the base directory under which the structure
-   * <ul>
-   * <li>inbound</li>
-   * <li>import</li>
-   * <li>archive</li>
-   * <li>export</li>
-   * </ul>
-   * is located.
-   */
   private Path baseDirectory;
 
   /**
    * The name of the <code>inbound</code> directory.
    */
   private Path inbound;
-
-  /**
-   * The name of the <code>importer</code> directory.
-   */
-  private Path importer;
-
-  /**
-   * The name of the <code>archive</code> directory.
-   */
-  private Path archive;
 
   /**
    * The name of the <code>export</code> directory.
@@ -58,8 +32,6 @@ public class ConfigurationLeiDaD {
     LOG.info("ConfigurationLeiDaD.ConfigurationLeiDaD");
     this.baseDirectory = null;
     this.inbound = null;
-    this.importer = null;
-    this.archive = null;
   }
 
   public Path getBaseDirectory() {
@@ -73,34 +45,6 @@ public class ConfigurationLeiDaD {
 
   public Path getInbound() {
     return Objects.requireNonNullElseGet(this.inbound, () -> this.baseDirectory.resolve(DEFAULT_INBOUND));
-  }
-
-  void setInbound(Path inbound) {
-    this.inbound = inbound;
-  }
-
-  public Path getImporter() {
-    return Objects.requireNonNullElseGet(this.importer, () -> this.baseDirectory.resolve(DEFAULT_IMPORTER));
-  }
-
-  void setImporter(Path importer) {
-    this.importer = importer;
-  }
-
-  public Path getArchive() {
-    return Objects.requireNonNullElseGet(this.archive, () -> this.baseDirectory.resolve(DEFAULT_ARCHIVE));
-  }
-
-  void setArchive(Path archive) {
-    this.archive = archive;
-  }
-
-  public Path getExport() {
-    return Objects.requireNonNullElseGet(this.export, () -> this.baseDirectory.resolve(DEFAULT_EXPORT));
-  }
-
-  void setExport(Path export) {
-    this.export = export;
   }
 
 }
